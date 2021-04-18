465cef1a1c631c1d6d63463b0f9fbdd462e12aec
aleperf <aleperf@users.noreply.github.com>
2021-04-18 11:27:03 +0200

    styled sign-up section

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,22 @@
 import "../scss/main.scss";
+
+const submitBtn = document.getElementById("sign-up-submit");
+const errorMessageEl = document.getElementById(" email-error-message");
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const submittedEmail = document.getElementById("email").value;
+  if (validateEmail(submittedEmail)) {
+    errorMessageEl.classList.remove("visible");
+  } else {
+    errorMessageEl.classList.add("visible");
+  }
+});
+
+const validateEmail = (submittedEmail) => {
+  const mailRegEx = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (submittedEmail.match(mailRegEx)) {
+    return true;
+  }
+  return false;
+};
